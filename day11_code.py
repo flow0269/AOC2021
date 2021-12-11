712b23439e698b71f645a29988904fde9e027624
Kreg <kflow7@gmail.com>
2021-12-10 23:16:41 -0700

    adding day11

--- day11_code.py
+++ day11_code.py
@@ -11,38 +11,38 @@ def in_bounds(y, x, o):
         return False
 
 
-def light_offsets(y, x, grid_, zeroed, total_flashes):
-    global offsets
+def light_offsets(y, x, grid_, zeroed):
+    global offsets, total_flashes
     for o in offsets:
         if in_bounds(y, x, o):
             if (y + o[0], x + o[1]) not in zeroed:
                 grid_[y + o[0]][x + o[1]] = (grid_[y + o[0]][x + o[1]] + 1) % 10
-
                 if grid_[y + o[0]][x + o[1]] == 0:
-                    total_flashes.append(1)
+                    total_flashes += 1
                     zeroed.add((y + o[0], x + o[1]))
-                    light_offsets(y + o[0], x + o[1], grid_, zeroed, total_flashes)
+                    light_offsets(y + o[0], x + o[1], grid_, zeroed)
 
 
-def increment(grid_, zeroed, total_flashes):
+def increment(grid_, zeroed):
+    global total_flashes
     for y, row in enumerate(grid_):
         for x, val in enumerate(row):
             if (y, x) not in zeroed:
                 grid_[y][x] = (val + 1) % 10
                 if grid_[y][x] == 0:
-                    total_flashes.append(1)
+                    total_flashes += 1
                     zeroed.add((y, x))
-                    light_offsets(y, x, grid_, zeroed, total_flashes)
+                    light_offsets(y, x, grid_, zeroed)
     return grid_
 
 
 zeroed = set()
-total_flashes = []
+total_flashes = 0
 for iteration_number in range(1000):
     zeroed.clear()
-    grid_ = increment(grid_, zeroed, total_flashes)
+    grid_ = increment(grid_, zeroed)
     if iteration_number == 99:
-        print("part1 answer", len(total_flashes))
+        print("part1 answer", total_flashes)
     if len(zeroed) == len(grid_) * len(grid_[0]):
         break
 
